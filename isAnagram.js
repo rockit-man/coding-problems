1ce3b011ee0c69a156bbf7395163530516d188b1
Nikhil Nambiar <nambiar.nikhil11@gmail.com>
2022-07-02 16:58:49 -0500

    string split function

--- isAnagram.js
+++ isAnagram.js
@@ -2,3 +2,15 @@ const isAnagram = (str1 , str2) => {
     return str1.toLowerCase().split('').sort().join('') === 
            str2.toLowerCase().split('').sort().join('')      
 }
+
+const stringSplit = (str, arr = []) => {
+    for(let i = 0; i < str.length; i++) {
+        arr.push(str[i])
+   }
+   return arr;
+}
+
+const isAnagramWithSplit = (str1, str2) => {
+    return stringSplit(str1.toLowerCase()).sort().join('') === 
+           stringSplit(str2.toLowerCase()).sort().join('')
+}

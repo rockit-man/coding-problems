ac41b16b0ec04c4dedab7db5ab2289ac00db7127
Nikhil Nambiar <nambiar.nikhil11@gmail.com>
2022-07-02 16:08:53 -0500

    consider casing

--- isAnagram.js
+++ isAnagram.js
@@ -1,3 +1,4 @@
-const isAnagram = (str1, str2) => {
-    return str1.split('').sort().join('') === str2.split('').sort().join('')  
+const isAnagram = (str1 , str2) => {
+    return str1.toLowerCase().split('').sort().join('') === 
+           str2.toLowerCase().split('').sort().join('')  
 }

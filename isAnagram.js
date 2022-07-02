342d0d6f419480a733918981eb58df63e7e11d1f
Nikhil Nambiar <nambiar.nikhil11@gmail.com>
2022-07-02 16:13:21 -0500

    take whitespace out

--- isAnagram.js
+++ isAnagram.js
@@ -1,5 +1,4 @@
 const isAnagram = (str1 , str2) => {
     return str1.toLowerCase().split('').sort().join('') === 
-           str2.toLowerCase().split('').sort().join('')  
-           
+           str2.toLowerCase().split('').sort().join('')      
 }

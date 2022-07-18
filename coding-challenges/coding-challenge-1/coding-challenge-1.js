2fb04ede4b893fb2e2bea1c7d57d9eff7252f04e
Jereme Dyson <79616995+rockit-man@users.noreply.github.com>
2022-07-18 11:56:06 -0500

    Create coding-challenge-1.js

---
+++ coding-challenges/coding-challenge-1/coding-challenge-1.js
@@ -0,0 +1,19 @@
+
+/* Create a function that takes in an array of words 
+as the parameter and pluralizes those words. In the array 
+of words will also be your first name. The function should 
+take in your first name and add "'s" to it. As a bonus,
+create a '.spec.js' file with unit tests. */
+
+/* Function should look something like this:
+
+function pluralizeWords(words) {
+  if word in words == Jereme (ignore case)
+    add "'s" to word
+  otherwise
+    for each word in words,
+      add a "s'"
+  return words
+ }
+ 
+ */

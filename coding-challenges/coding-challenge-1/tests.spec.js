9e8ac8e15ef175b1573734cc8f5b58db5e5fce9e
Jereme Dyson <79616995+rockit-man@users.noreply.github.com>
2022-07-18 21:12:33 -0500

    Create tests.spec.js

---
+++ coding-challenges/coding-challenge-1/tests.spec.js
@@ -0,0 +1,8 @@
+// import { pluralizeWords } from "./coding-challenge-1";
+
+// describe('coding-challenge-1 tests', () => {
+  
+//   test('is word in words Jereme', () => {
+//     expect(pluralizeWords([apples, oranges, Jereme]).toBeTruthy();
+  
+// }

59119bb644f768abb3bc5af9244f611a5dccda1b
rockit-man <demetrius.esq@gmail.com>
2022-07-28 15:24:30 -0500

    Test for whitespace added and passes

--- coding-challenges/coding-challenge-1/coding-challenge-1.spec.js
+++ coding-challenges/coding-challenge-1/coding-challenge-1.spec.js
@@ -7,5 +7,18 @@ describe('coding-challenge-1 tests', () => {
     expect(pluralizeWords(['apple', 'orange', 'jereme'])).toStrictEqual(['apples', 'oranges', "Jereme's"]);
     expect(pluralizeWords(['apple', 'orange', 'Nikhil'])).toStrictEqual(['apples', 'oranges', 'Nikhils']);
   });
+
+  test('does word in words have any whitespace', () => {
+    expect(pluralizeWords(['apple', 'orange', 'Jereme']).indexOf(' ')).toBeFalsy;
+    expect(pluralizeWords(['appl e', 'oranges', 'Jereme']).indexOf(' ')).toBeTruthy;
+  });
+
+  // test('does word in words have an s on the end already', () => {
+
+  // });
+
+  // test('does the name in words have white space', () => {
   
-})
+  // });
+  
+});

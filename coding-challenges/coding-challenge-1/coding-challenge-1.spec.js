4be01b0a0f2e415ef5ca5d75ed502cbaf326aab3
rockit-man <demetrius.esq@gmail.com>
2022-07-29 11:02:27 -0500

    Additional test cases have been added but have not been implemented into the pluralizeWords function. Waiting for review.

--- coding-challenges/coding-challenge-1/coding-challenge-1.spec.js
+++ coding-challenges/coding-challenge-1/coding-challenge-1.spec.js
@@ -2,23 +2,23 @@ import { pluralizeWords } from "./coding-challenge-1";
 
 describe('coding-challenge-1 tests', () => {
   
-  test('is word in words Jereme', () => {
+test('is word in words Jereme', () => {
     expect(pluralizeWords(['apple', 'orange', 'Jereme'])).toStrictEqual(['apples', 'oranges', "Jereme's"]);
     expect(pluralizeWords(['apple', 'orange', 'jereme'])).toStrictEqual(['apples', 'oranges', "Jereme's"]);
     expect(pluralizeWords(['apple', 'orange', 'Nikhil'])).toStrictEqual(['apples', 'oranges', 'Nikhils']);
   });
 
   test('does word in words have any whitespace', () => {
-    expect(pluralizeWords(['apple', 'orange', 'Jereme']).indexOf(' ')).toBeFalsy;
-    expect(pluralizeWords(['appl e', 'oranges', 'Jereme']).indexOf(' ')).toBeTruthy;
+    expect(pluralizeWords(['apple', 'orange', 'Jereme'])).toStrictEqual(['apples', 'oranges', "Jereme's"]);
+    expect(pluralizeWords(['white grape', 'oranges', 'Jereme'])).toStrictEqual(['white grapes', 'oranges', "Jereme's"]);
   });
 
-  // test('does word in words have an s on the end already', () => {
+  test('does word in words have an s on the end already', () => {
+    expect(pluralizeWords(['apples', 'oranges', 'Jereme'])).toStrictEqual(['apples', 'oranges', "Jereme's"]);
+  });
 
-  // });
-
-  // test('does the name in words have white space', () => {
-  
-  // });
+  test('does the name in words have white space', () => {
+    expect(pluralizeWords(['apple', 'oranges', 'Metta Sandiford-Artest'])).toStrictEqual(['apples', 'oranges', "Metta Sandiford-Artest"]);
+  });
   
 });

f06bd99eed8fb8e3f2f385609dab79e0a2b0db84
rockit-man <demetrius.esq@gmail.com>
2022-08-04 14:47:10 -0500

    Added an additional solution using arrow functions and Array.map method

--- coding-challenges/interview-challenge-1.js
+++ coding-challenges/interview-challenge-1.js
@@ -1,5 +1,6 @@
 let birbArr = ['Northern Cardinal', 'Gray Catbird', 'Carolina Wren'];
 
+// using a for loop
 const smolBirbs = (arr) => {
     let smolArr = [];
     for (let i = 0; i < arr.length; i++) {
@@ -9,10 +10,13 @@ const smolBirbs = (arr) => {
     return smolArr;
 };
 
-console.log(smolBirbs(birbArr));
-
 /* Took me almost 15 additional minutes, but I finally figured it out.
 I was focusing on the lowercase string function because that was the
 line the error was catching on. You told me to look into looping through
 arrays again, so that's where I started. I didn't set up my for loop
 correctly and I needed to update my arr[i] values. */
+
+//using arrow functions and the Array.map method
+const smolBirbs = (arr) => {
+    return arr.map(b => b.toLowerCase());
+};
